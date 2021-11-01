88ebda1b62edd1c03b884dc157d591c1582333d7
46Rabbit <83830109+46rabbit@users.noreply.github.com>
2021-11-01 13:36:25 +0900

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string initialRoute = "";
+        List<string> fileList = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            initialRoute = GetApplicationRoot();
+            initialRoute = System.IO.Path.Combine(initialRoute, "cosaSave");
+
+            DirectoryInfo fi = new DirectoryInfo(initialRoute);
+            if (!fi.Exists)
+            {
+                fi.Create();
+            }
+
+            refreashList();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.ShowDialog();
+
+            if (window1.DialogResult.HasValue && window1.DialogResult.Value)
+            {
+                refreashList();
+            }
+        }
+
+        private void refreashList()
+        {
+            listBox.Items.Clear();
+            fileList.Clear();
+
+            string[] fileArray = Directory.GetFiles(initialRoute);
+
+            foreach(var item in fileArray)
+            {
+                fileList.Add(item);
+                listBox.Items.Add(Path.GetFileName(item));
+            }
+        }
+
+        public string GetApplicationRoot()
+        {
+            var exePath = System.IO.Path.GetDirectoryName(System.Reflection
+                              .Assembly.GetExecutingAssembly().CodeBase);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return appRoot;
+        }
+
+        private void removeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            fileList.RemoveAt(listBox.SelectedIndex-1);
+
+            FileInfo fileInfo = new FileInfo(fileList[listBox.SelectedIndex-1]);
+            fileInfo.Delete();
+
+            refreashList();
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string initialRoute = "";
+
+        public Window1()
+        {
+            InitializeComponent();
+            initialRoute = GetApplicationRoot();
+
+            initialRoute = System.IO.Path.Combine(initialRoute, "cosaSave");
+        }
+
+        private void Okbtn_Click(object sender, RoutedEventArgs e)
+        {
+            saveFiletoPC();
+            this.DialogResult = true;
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void saveFiletoPC()
+        {
+            string currentFileRoute = System.IO.Path.Combine(initialRoute, fileNameTxtBox.Text + ".cosa");
+            FileInfo fi = new FileInfo(currentFileRoute);
+
+            if(fi.Exists)
+            {
+                if(MessageBox.Show("The file you want to create already exists.\nDo you want to overwirte it?", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Stop) == MessageBoxResult.Yes)
+                {
+                    fi.Delete();
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            using (StreamWriter file = new StreamWriter(currentFileRoute))
+            {
+                file.Write(contentTxtbox.Text);
+            }
+        }
+
+        public string GetApplicationRoot()
+        {
+            var exePath = System.IO.Path.GetDirectoryName(System.Reflection
+                              .Assembly.GetExecutingAssembly().CodeBase);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return appRoot;
+        }
+    }
+}
